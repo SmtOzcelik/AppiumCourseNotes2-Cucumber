524c4dc1f5d3bba0a2a5a4d7954f738369612d6b
SmtOzcelik <smtab142790@gmail.com>
2023-09-25 19:28:22 +0300

    25/09/2023

---
+++ src/test/java/stepDefinitions/ApiDemosSteps.java
@@ -0,0 +1,55 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+public class ApiDemosSteps {
+    @Given("App yuklensin")
+    public void appYuklensin() {
+    }
+
+    @And("kullanici ana ekranda")
+    public void kullaniciAnaEkranda() {
+    }
+
+    @And("kullanici API Demos butununa tikladi")
+    public void kullaniciAPIDemosButununaTikladi() {
+    }
+
+    @Then("kullanici API Demos ekraninda")
+    public void kullaniciAPIDemosEkraninda() {
+    }
+
+    @And("kullanici Preference butununa tikladi")
+    public void kullaniciPreferenceButununaTikladi() {
+    }
+
+    @Then("kullanici Preference ekraninda")
+    public void kullaniciPreferenceEkraninda() {
+    }
+
+    @And("kullanici Preference dependencies tikladi")
+    public void kullaniciPreferenceDependenciesTikladi() {
+    }
+
+    @And("kullanici WiFi check box secmis olacak")
+    public void kullaniciWiFiCheckBoxSecmisOlacak() {
+    }
+
+    @And("kullanici WiFi Settings tikladi")
+    public void kullaniciWiFiSettingsTikladi() {
+    }
+
+    @Then("WiFi setting popup geldi")
+    public void wifiSettingPopupGeldi() {
+    }
+
+    @And("kullanici {string} yazdi")
+    public void kullaniciYazdi(String arg0) {
+    }
+
+    @And("kullanci ok butonuna tikladi")
+    public void kullanciOkButonunaTikladi() {
+    }
+}
